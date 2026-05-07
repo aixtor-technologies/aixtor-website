760845dbf3606518d0123572b171bf2da5924998
PrincekatrodiyaAIX <prince.katrodiya@aixtor.com>
2026-05-07 12:05:15 +0530

    image config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "https://dev.aixtor.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
